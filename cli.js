794b95f91ebee47b10165ff56f9df33e7db71bd2
Kenny Murillo <kenmury@email.unc.edu>
2023-04-27 00:52:50 -0400

    dependencies

--- cli.js
+++ cli.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
+// Check dependencies 
+
+import minimist from 'minimist'
+import node-fetch from 'node-fetch'
+import moment-timezone from 'moment-timezone'
+
+
+
 console.log("hello");
