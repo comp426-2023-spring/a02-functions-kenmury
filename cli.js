6b3d87477d351ea591e75236bd591f882e8c3ab7
Kenny Murillo <kenmury@email.unc.edu>
2023-04-27 01:03:36 -0400

    -h

--- cli.js
+++ cli.js
@@ -8,6 +8,18 @@ import moment from 'moment-timezone'
 
 var args = minimist(process.argv.slice(2))
 
+if (args.h) {
+    console.log(`
+    Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+        -h            Show this help message and exit.
+        -n, -s        Latitude: N positive; S negative.
+        -e, -w        Longitude: E positive; W negative.
+        -z            Time zone: uses tz.guess() from moment-timezone by default.
+        -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+        -j            Echo pretty JSON from open-meteo API and exit.
+    `)
+}
 
 
-console.log(args);
+
+// console.log(args);
